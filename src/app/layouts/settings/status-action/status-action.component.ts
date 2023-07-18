d97cd872cea5fc7b4577990fd3ee507e9ac99551
reda-lazrek <123340557+reda-lazrek@users.noreply.github.com>
2023-07-18 10:57:54 +0100

    R.L: updated general section

--- src/app/layouts/settings/status-action/status-action.component.ts
+++ src/app/layouts/settings/status-action/status-action.component.ts
@@ -1,8 +1,9 @@
+import { MessageService } from "primeng/api";
+import { ActionStatusService } from "./../../../shared/services/api/action-status.service";
 import { Component, OnInit } from "@angular/core";
 import { NgxSpinnerService } from "ngx-spinner";
 import { Subscription } from "rxjs";
 import { StatusAction } from "../../../shared/models";
-
 
 @Component({
   selector: "app-status-action",
@@ -25,7 +26,9 @@ export class StatusActionComponent implements OnInit {
   subscriptions = new Subscription();
 
   constructor(
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private actionStatusService: ActionStatusService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
@@ -36,6 +39,21 @@ export class StatusActionComponent implements OnInit {
 
   loadData(search: string = "") {
     this.spinner.show();
+    this.subscriptions.add(
+      this.actionStatusService.findAll().subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => {
+          this.spinner.hide();
+          this.messageService.add({
+            severity: "error",
+            summary: "Erreur",
+            detail: JSON.stringify(error),
+          });
+        }
+      )
+    );
   }
 
   reset() {
